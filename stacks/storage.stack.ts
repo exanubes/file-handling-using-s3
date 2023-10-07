a03abaee114473ddf26bb08136117d36c2f1213c
Exanubes <blog@exanubes.com>
2023-10-07 19:05:19 +0200

    Add sns topic listener for s3 object restored events

--- stacks/storage.stack.ts
+++ stacks/storage.stack.ts
@@ -1,8 +1,22 @@
-import { Bucket, StackContext } from 'sst/constructs';
-import {BlockPublicAccess, HttpMethods, StorageClass} from 'aws-cdk-lib/aws-s3';
-import {Duration} from "aws-cdk-lib";
+import { Bucket, StackContext, Topic } from 'sst/constructs';
+import { BlockPublicAccess, HttpMethods, StorageClass } from 'aws-cdk-lib/aws-s3';
+import { Duration } from 'aws-cdk-lib';
 
 export function StorageStack({ stack }: StackContext) {
+	const topic = new Topic(stack, 'objectRestored', {
+		subscribers: {
+			notificationEmail: {
+				type: 'function',
+				function: {
+					functionName: 'object-restored-notification-email',
+					handler: 'packages/functions/src/object-restored-notification-email.handler',
+					architecture: 'arm_64',
+					permissions: ['ses', 's3']
+				}
+			}
+		}
+	});
+
 	const bucket = new Bucket(stack, 'uploads', {
 		name: 'exanubes-upload-bucket-sst',
 		cors: [
@@ -33,6 +47,13 @@ export function StorageStack({ stack }: StackContext) {
 						]
 					}
 				]
+			}
+		},
+		notifications: {
+			GlacierObjectRestored: {
+				events: ['object_restore_completed'],
+				type: 'topic',
+				topic
 			}
 		}
 	});
