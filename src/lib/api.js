c061894b220fc1e0d16db3f3aac7a3c0e5231ac1
Exanubes <blog@exanubes.com>
2023-10-28 14:05:36 +0200

    Split file into multiple parts and upload using signed urls

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,5 @@
+import { MINIMUM_PART_SIZE } from '$lib/const.js';
+
 /**
  * @description sends a http POST request to save the file reference to the database
  * @param {{files: {name: string; key: string}[]}} body
@@ -25,4 +27,30 @@ export function confirmNewVersion(id, body) {
 		},
 		body: JSON.stringify(body)
 	});
+}
+
+/**
+ * @description slice file into parts and upload each part using signed urls
+ * @param {Blob} file
+ * @param {string[]} signedUrls
+ * */
+export async function uploadPartsToS3(file, signedUrls) {
+	return Promise.all(
+		signedUrls.map(async (signedUrl, index) => {
+			const start = index * MINIMUM_PART_SIZE;
+			const end = (index + 1) * MINIMUM_PART_SIZE;
+			const isLastPart = index === signedUrls.length - 1;
+			const part = isLastPart ? file.slice(start) : file.slice(start, end);
+			const response = await fetch(signedUrl, {
+				method: 'PUT',
+				body: part
+			});
+
+			/**@type {import('@aws-sdk/client-s3').CompletedPart}*/
+			return {
+				ETag: response.headers.get('etag'),
+				PartNumber: index + 1
+			};
+		})
+	);
 }
