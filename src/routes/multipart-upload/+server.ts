9ded4cef76bd4a2284d366016f35e57dd3d935c5
Exanubes <blog@exanubes.com>
2023-10-28 14:24:53 +0200

    Add complete multipart upload endpoint

--- src/routes/multipart-upload/+server.ts
+++ src/routes/multipart-upload/+server.ts
@@ -1,2 +1,19 @@
 import type { RequestHandler } from './$types';
-export const POST = async function POST({ request }) {} satisfies RequestHandler;
+import { completeMultipartUpload } from '$lib/s3';
+import { Bucket } from 'sst/node/bucket';
+import { createDocument } from '$lib/documents';
+import { json } from '@sveltejs/kit';
+export const POST = async function POST({ request }) {
+	const { uploadId, key, name, uploadedParts } = await request.json();
+
+	await completeMultipartUpload({
+		bucket: Bucket.uploads.bucketName,
+		parts: uploadedParts,
+		uploadId,
+		key
+	});
+	const id = await createDocument({ key, name });
+	const response = { id, key, name };
+
+	return json({ message: 'created', data: response }, { status: 201 });
+} satisfies RequestHandler;
