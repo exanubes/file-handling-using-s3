49714e7c379a08ee5c696fc4208eef808c8da2db
Exanubes <blog@exanubes.com>
2023-10-28 14:20:53 +0200

    Define upload action

--- src/routes/multipart-upload/+page.server.ts
+++ src/routes/multipart-upload/+page.server.ts
@@ -1,5 +1,7 @@
 import { listDocuments } from '$lib/documents';
 import type { Actions, PageServerLoad } from './$types';
+import { getMultipartUrls, startMultipartUpload } from '$lib/s3';
+import { Bucket } from 'sst/node/bucket';
 
 export const load = async function load() {
 	const documents = await listDocuments();
@@ -12,7 +14,25 @@ export const load = async function load() {
 export const actions: Actions = {
 	async upload({ request }) {
 		const data = await request.formData();
+		const parts = await data.get('parts');
+		const key = crypto.randomUUID();
 
-		return {};
+		const response = await startMultipartUpload({
+			bucket: Bucket.uploads.bucketName,
+			key
+		});
+
+		const urls = await getMultipartUrls({
+			bucket: Bucket.uploads.bucketName,
+			uploadId: String(response.UploadId),
+			parts: Number(parts),
+			key
+		});
+
+		return {
+			urls,
+			key,
+			uploadId: response.UploadId
+		};
 	}
 };
